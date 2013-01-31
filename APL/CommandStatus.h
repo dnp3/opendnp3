eb8e04a8b7bda2147136657ed425c9ae509f51e8
J Adam Crain <jadamcrain@gmail.com>
2013-01-31 12:05:18 -0500

    started changing types to be more explicit and in line with the standard

---
+++ APL/CommandStatus.h
@@ -0,0 +1,65 @@
+
+//
+// Licensed to Green Energy Corp (www.greenenergycorp.com) under one or
+// more contributor license agreements. See the NOTICE file distributed
+// with this work for additional information regarding copyright ownership.
+// Green Energy Corp licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This file was forked on 01/01/2013 by Automatak, LLC and modifications
+// have been made to this file. Automatak, LLC licenses these modifications to
+// you under the GNU Affero General Public License Version 3.0
+// (the "Additional License"). You may not use these modifications except in
+// compliance with the additional license. You may obtain a copy of the Additional
+// License at
+//
+// http://www.gnu.org/licenses/agpl.html
+//
+// Contact Automatak, LLC for a commercial license to these modifications
+//
+#ifndef __COMMAND_STATUS_H_
+#define __CONTROL_STATUS_H_
+
+#include <cstdint>
+#include <string>
+
+namespace apl
+{
+
+/**
+ * When a command is recieved from a master the application sends a code to
+ * indicate if it was successfull or if not what class of error was
+ * encountered.  Each code has a description that indicates its customary
+ * meaning.
+ */
+enum CommandStatus {
+	CS_SUCCESS = 0,			//!< command was successfully recieved and handled
+	CS_TIMEOUT = 1,			//!< command timedout before completing
+	CS_NO_SELECT = 2,		//!< command requires being selected before operate, configuration issue
+	CS_FORMAT_ERROR = 3,	//!< bad control code or timing values
+	CS_NOT_SUPPORTED = 4,	//!< command is not implemented
+	CS_ALREADY_ACTIVE = 5,	//!< command is allready in progress or its allready in that mode
+	CS_HARDWARE_ERROR = 6,	//!< something is stopping the command, often a local/remote interlock
+	CS_LOCAL = 7,			//!< the function goverened by the control is in local only control
+	CS_TOO_MANY_OPS = 8,	//!< the command has been done too often and has been throttled
+	CS_NOT_AUTHORIZED = 9,	//!< the command was rejected because the device denied it or an RTU intercepted it
+	CS_UNDEFINED = 127		//!< 10 to 126 are currently reserved
+};
+
+CommandStatus ByteToCommandStatus(uint8_t aField);
+std::string ToString(CommandStatus aType);
+
+}
+
+/* vim: set ts=4 sw=4: */
+
+#endif
--- APL/ControlRelayOutputBlock.cpp
+++ APL/ControlRelayOutputBlock.cpp
@@ -26,7 +26,7 @@
 //
 // Contact Automatak, LLC for a commercial license to these modifications
 //
-#include "CommandTypes.h"
+#include "ControlRelayOutputBlock.h"
 #include "PackingUnpacking.h"
 #include "Exception.h"
 
@@ -37,26 +37,6 @@ namespace apl
 {
 
 #define MACRO_CASE_DECLARE(type) case(type): return type;
-
-CommandStatus ByteToCommandStatus(uint8_t aField)
-{
-	switch(aField) {
-		MACRO_CASE_DECLARE(CS_SUCCESS)
-		MACRO_CASE_DECLARE(CS_TIMEOUT)
-		MACRO_CASE_DECLARE(CS_NO_SELECT)
-		MACRO_CASE_DECLARE(CS_FORMAT_ERROR)
-		MACRO_CASE_DECLARE(CS_NOT_SUPPORTED)
-		MACRO_CASE_DECLARE(CS_ALREADY_ACTIVE)
-		MACRO_CASE_DECLARE(CS_HARDWARE_ERROR)
-		MACRO_CASE_DECLARE(CS_LOCAL)
-		MACRO_CASE_DECLARE(CS_TOO_MANY_OPS)
-		MACRO_CASE_DECLARE(CS_NOT_AUTHORIZED)
-
-	default:
-		return CS_UNDEFINED;
-	}
-
-}
 
 ControlCode ByteToControlCode(uint8_t aField)
 {
@@ -76,23 +56,6 @@ ControlCode ByteToControlCode(uint8_t aField)
 
 #define TO_STRING_CASE(c) case (c): return #c;
 
-std::string ToString(CommandStatus aType)
-{
-	switch(aType) {
-		TO_STRING_CASE(CS_SUCCESS)
-		TO_STRING_CASE(CS_TIMEOUT)
-		TO_STRING_CASE(CS_NO_SELECT)
-		TO_STRING_CASE(CS_FORMAT_ERROR)
-		TO_STRING_CASE(CS_NOT_SUPPORTED)
-		TO_STRING_CASE(CS_ALREADY_ACTIVE)
-		TO_STRING_CASE(CS_HARDWARE_ERROR)
-		TO_STRING_CASE(CS_LOCAL)
-		TO_STRING_CASE(CS_TOO_MANY_OPS)
-		TO_STRING_CASE(CS_NOT_AUTHORIZED)
-	default:
-		return "Unknown";
-	}
-}
 std::string ToString(ControlCode aType)
 {
 	switch(aType) {
@@ -128,70 +91,6 @@ std::string BinaryOutput::ToString() const
 	oss << "BinaryOutput - function: " << apl::ToString(static_cast<ControlCode>(mRawCode));
 	oss << " count: " << static_cast<size_t>(mCount) << " on: " << mOnTimeMS << " off: " << mOffTimeMS;
 	return oss.str();
-}
-
-Setpoint::Setpoint() : mEncodingType(SPET_UNSET) {}
-
-Setpoint::Setpoint(int16_t aValue) :	
-	mStatus(CS_SUCCESS),
-	mValue(aValue),
-	mEncodingType(SPET_AUTO_INT)
-{}
-
-Setpoint::Setpoint(int32_t aValue) :
-	mStatus(CS_SUCCESS),
-	mValue(aValue),
-	mEncodingType(SPET_AUTO_INT)
-{}
-
-Setpoint::Setpoint(double aValue) :
-	mStatus(CS_SUCCESS),
-	mValue(aValue),
-	mEncodingType(SPET_AUTO_DOUBLE)
-{}
-
-std::string Setpoint::ToString() const
-{
-	std::ostringstream oss;
-	oss << "Setpoint - value: " << mValue;
-	return oss.str();
-}
-
-void Setpoint::SetValue(double aValue)
-{
-	mValue = aValue;
-	if(mEncodingType == SPET_UNSET) {
-		mEncodingType = SPET_AUTO_DOUBLE;
-	}
-}
-void Setpoint::SetValue(int32_t aValue)
-{
-	mValue = static_cast<double>(aValue);
-	if(mEncodingType == SPET_UNSET) {
-		mEncodingType = SPET_AUTO_INT;
-	}
-}
-
-double Setpoint::GetValue() const
-{
-	assert(mEncodingType != SPET_UNSET);
-	return mValue;
-}
-
-SetpointEncodingType Setpoint::GetOptimalEncodingType() const
-{
-	assert(mEncodingType != SPET_UNSET);
-	if(mEncodingType == SPET_AUTO_INT) {
-		if(mValue <= Int16LE::Max && mValue >= Int16LE::Min) return SPET_INT16;
-		return SPET_INT32;
-	}
-	if(mEncodingType == SPET_AUTO_DOUBLE) {
-		if(mValue <= SingleFloat::Max && mValue >= SingleFloat::Min) return SPET_FLOAT;
-		return SPET_DOUBLE;
-	}
-	// if its not one of the auto types that means its been explictly set
-	// so we should use that type.
-	return mEncodingType;
 }
 
 
