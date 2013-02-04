704d62ff87b1bb90a8b7dbfe4b56291ea661fd0e
J Adam Crain <jadamcrain@gmail.com>
2013-02-04 10:22:58 -0500

    Added support for scala and scalatest

---
+++ java/maven/bindings/src/test/scala/com.automatak.dnp3.impl/DNP3ManagerTestSuite.scala
@@ -0,0 +1,12 @@
+package com.automatak.dnp3.impl
+
+import org.scalatest.fixture
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+import org.scalatest.matchers.ShouldMatchers
+
+@RunWith(classOf[JUnitRunner])
+class DNP3ManagerTestSuite extends fixture.FunSuite with ShouldMatchers {
+
+
+}
