a227e588deb3800feb49ba49cc98734ebebc6cfd
J Adam Crain <jadamcrain@gmail.com>
2013-03-25 09:40:01 -0400

    Fixed a conformance issue

--- cpp/src/opendnp3/Slave.h
+++ cpp/src/opendnp3/Slave.h
@@ -287,7 +287,9 @@ void Slave::RespondToCommands(const StreamObject<T>* apObj, ObjectReadIterator& 
 		}
 		else {
 			val.mStatus = CommandHandler(val, index);
-			if(val.mStatus == CS_NOT_SUPPORTED) this->mRspIIN.SetParameterError(true);
+			if(val.mStatus == CS_NOT_SUPPORTED) {
+				this->mRspIIN.SetObjectUnknown(true);//SetParameterError(true);
+			}
 		}
 		i.SetIndex(index);
 		apObj->Write(*i, val);
