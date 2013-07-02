46c091788221d000ace2484d99d32b868cbfef30
jadamcrain <jadamcrain@gmail.com>
2013-07-02 18:34:41 -0400

    Finished with the Apache licensing

--- java/cpp/ConfigReader.cpp
+++ java/cpp/ConfigReader.cpp
@@ -4,11 +4,11 @@
  * Licensed to Automatak, LLC (www.automatak.com) under one or more
  * contributor license agreements. See the NOTICE file distributed with this
  * work for additional information regarding copyright ownership. Automatak, LLC
- * licenses this file to you under the GNU Affero General Public License
- * Version 3.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * licenses this file to you under the Apache License Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
  *
- * http://www.gnu.org/licenses/agpl.html
+ * http://www.apache.org/licenses/LICENSE-2.0.html
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -16,7 +16,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 #include "ConfigReader.hpp"
 
 
