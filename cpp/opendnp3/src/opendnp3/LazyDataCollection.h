112367fda6f7a4d912e586a1563e727ecb002694
jadamcrain <jadamcrain@gmail.com>
2013-12-05 17:10:28 -0500

    Working lazy collection

---
+++ cpp/opendnp3/src/opendnp3/LazyDataCollection.h
@@ -0,0 +1,112 @@
+/**
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or
+ * more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Green Energy Corp licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This project was forked on 01/01/2013 by Automatak, LLC and modifications
+ * may have been made to this file. Automatak, LLC licenses these modifications
+ * to you under the terms of the License.
+ */
+#ifndef __LAZY_DATA_COLLECTION_H_
+#define __LAZY_DATA_COLLECTION_H_
+
+#include <functional>
+
+namespace opendnp3
+{
+
+template <class T>
+class LazyDataCollection
+{
+	public:
+
+		class const_iterator : public std::iterator<std::random_access_iterator_tag, T> 
+		{
+			openpal::ReadOnlyBuffer mBuffer;
+			std::function<T (openpal::ReadOnlyBuffer&)> mReadType;
+			size_t mCount;
+			size_t mNumValues;
+			
+			public:
+		
+			const_iterator(const openpal::ReadOnlyBuffer& arBuffer, const std::function<T (openpal::ReadOnlyBuffer&)>& aReadType, size_t aNumValues):
+				mBuffer(arBuffer),
+				mReadType(aReadType),
+				mCount(0),
+				mNumValues(aNumValues)
+			{
+				
+			}
+				
+			bool operator !=(const const_iterator& rhs)
+			{
+				return mCount < mNumValues;
+			}
+
+			void operator ++()
+			{
+				assert(mCount < mNumValues);
+				mCount += 1;
+			}
+
+			T operator *()
+			{
+				return mReadType(mBuffer);
+			}
+
+			const_iterator(const const_iterator& rhs) : 
+				mBuffer(rhs.mBuffer),
+				mReadType(rhs.mReadType),
+				mCount(rhs.mCount),
+				mNumValues(rhs.mNumValues)
+			{
+				
+			}			 
+		};
+
+
+
+		LazyDataCollection(const openpal::ReadOnlyBuffer& arBuffer, const std::function<T (openpal::ReadOnlyBuffer&)>& aReadType, size_t aCount):
+			mBuffer(arBuffer),
+			mConvert(aReadType),
+			mCount(aCount)
+		{
+		
+		}
+
+		const_iterator begin() const
+		{
+			return const_iterator(mBuffer, mConvert, mCount);
+		}
+		
+		const_iterator end() const
+		{
+			return const_iterator(mBuffer, mConvert, 0);
+		}
+
+	private:
+		
+		LazyDataCollection();
+		LazyDataCollection(const LazyDataCollection&);
+
+		openpal::ReadOnlyBuffer mBuffer;
+		std::function<T (openpal::ReadOnlyBuffer&)> mConvert;
+		size_t mCount;
+};
+
+
+
+}
+
+#endif
---
+++ cpp/opendnp3tests/src/TestLazyDataCollection.cpp
@@ -0,0 +1,54 @@
+/**
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or
+ * more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Green Energy Corp licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This project was forked on 01/01/2013 by Automatak, LLC and modifications
+ * may have been made to this file. Automatak, LLC licenses these modifications
+ * to you under the terms of the License.
+ */
+#include <boost/test/unit_test.hpp>
+
+#include "TestHelpers.h"
+#include "BufferHelpers.h"
+
+#include <opendnp3/LazyDataCollection.h>
+#include <opendnp3/objects/Group30.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace openpal;
+using namespace opendnp3;
+
+BOOST_AUTO_TEST_SUITE(LazyDataCollectionTestSuite)
+
+BOOST_AUTO_TEST_CASE(ReadsSimpleTypes)
+{
+	HexSequence hex("AB 01 00 CD 02 00");
+		
+	LazyDataCollection<Group30Var2> collection(hex.ToReadOnly(), Group30Var2::Read, 2);
+
+	std::vector<Group30Var2> vec;
+	for(auto m: collection) {		
+		vec.push_back(m);
+	}
+	BOOST_REQUIRE_EQUAL(2, vec.size());
+	BOOST_REQUIRE_EQUAL(1, vec[0].value);
+	BOOST_REQUIRE_EQUAL(0xAB, vec[0].flags);
+	BOOST_REQUIRE_EQUAL(2, vec[1].value);
+	BOOST_REQUIRE_EQUAL(0xCD, vec[1].flags);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
