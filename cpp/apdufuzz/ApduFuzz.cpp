53ea7a8c587ef57debf427ad2767e82c06e3c3e5
jadamcrain <jadamcrain@gmail.com>
2013-12-08 02:19:29 -0500

    added brute force random memory fuzzer

---
+++ cpp/apdufuzz/ApduFuzz.cpp
@@ -0,0 +1,111 @@
+/**
+ * Licensed to Green Energy Corp (www.greenenergycorp.com) under one or
+ * more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Green Energy Corp licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This project was forked on 01/01/2013 by Automatak, LLC and modifications
+ * may have been made to this file. Automatak, LLC licenses these modifications
+ * to you under the terms of the License.
+ */
+
+#include <opendnp3/APDUParser.h>
+
+#include <random>
+#include <iostream>
+#include <vector>
+#include <thread>
+
+#include "ResultSet.h"
+
+using namespace std;
+using namespace openpal;
+using namespace opendnp3;
+
+class Handler : public IAPDUHeaderHandler
+{
+	public:
+
+		virtual void AllObjects(GroupVariation gv) override
+		{}
+
+		void OnEventData(const LazyCollection<IndexedValue<Binary>>& meas) override
+		{ for(auto b: meas); }
+
+		void OnStaticData(uint32_t aStartIndex, const LazyCollection<Binary>& meas)  override
+		{ for(auto b: meas); }
+};
+
+class Fuzzer
+{
+	public:
+
+	Fuzzer(size_t iter, size_t s) : seed(s), iterations(iter)
+	{}
+
+	static const size_t MAX_SIZE = 100;
+
+	void Run()
+	{
+		Handler h;
+		std::mt19937 gen;
+		gen.seed(seed);
+		std::uniform_int_distribution<size_t> size(0, MAX_SIZE);
+		std::uniform_int_distribution<uint8_t> value(0x00, 0xFF);
+		
+		for(size_t i = 0; i<iterations; ++i)
+		{
+			size_t count  = size(gen);
+			for(size_t i = 0; i < count; ++i) buffer[i] = value(gen);
+			ReadOnlyBuffer rb(buffer, count);
+			APDUParser::Result result = APDUParser::ParseHeaders(rb, h);
+			results.Update(result);
+		}	
+	}
+
+	ResultSet results;	
+
+	private:
+
+	size_t seed;
+	size_t iterations;		
+	uint8_t buffer[MAX_SIZE];
+};
+
+int main(int argc, char* argv[])
+{
+	size_t iterations = 10*1000*1000;
+	size_t concurrency = std::thread::hardware_concurrency();
+
+	std::vector<Fuzzer> fuzzers;
+	for(size_t i = 0;  i <  concurrency; ++i) fuzzers.push_back(Fuzzer(iterations, i+1));
+	
+	std::vector<std::thread*> threads;
+	for(auto& f: fuzzers) {
+		auto pThread = new std::thread([&]() { f.Run(); });
+		threads.push_back(pThread);
+	}
+	
+	cout << "running" << endl;
+
+	for(auto pThread: threads) pThread->join();	
+
+	ResultSet rs;
+	for(auto& f: fuzzers) rs.Merge(f.results);
+
+	cout << "complete" << endl;
+
+	for(auto pThread: threads) delete pThread;
+
+	return 0;
+}
