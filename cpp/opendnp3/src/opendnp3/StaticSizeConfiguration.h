0e41c2ba63aba04305828613f14172daa7bd1218
Adam Crain <jadamcrain@gmail.com>
2014-04-07 15:54:15 -0400

    rearranged asserts to follow the definitions

--- cpp/opendnp3/src/opendnp3/StaticSizeConfiguration.h
+++ cpp/opendnp3/src/opendnp3/StaticSizeConfiguration.h
@@ -33,8 +33,7 @@ namespace sizes
 // the maximum number of static read object/variation records that can be in any READ request
 static const uint16_t MAX_READ_REQUESTS = 16;
 
-// a safeguard parameter to project from DoS attacks
-// this really only applies to "empty strings"
+// a safeguard parameter to project from DoS attacks, this really only applies to "empty strings"
 static const uint32_t MAX_OBJECTS_PER_APDU = 32768;
 
 // the maximum number of event read object/variation records that can be in any READ request
@@ -45,26 +44,29 @@ static const uint8_t MAX_COMMAND_QUEUE_SIZE = 16;
 
 // the maximum size of a transmitted APDU
 static const uint32_t MAX_TX_APDU_SIZE = 2048;
-static_assert(MAX_TX_APDU_SIZE >= 100, "APDU tx buffer size must be at least 100");
 
 // the maximum size of a received APDU
 static const uint32_t MAX_RX_APDU_SIZE = 2048;
-static_assert(MAX_RX_APDU_SIZE >= 249, "APDU rx buffer size must be at least 249 bytes");
 
 // the default APDU buffer size
 static const uint32_t DEFAULT_APDU_BUFFER_SIZE = MAX_TX_APDU_SIZE;
-static_assert(DEFAULT_APDU_BUFFER_SIZE <= MAX_TX_APDU_SIZE, "default buffer size must be less than the maximum");
 
 // The maximum number of stacks that can associate to a link layer router
 static const uint16_t MAX_STACKS_PER_CHANNEL = 16;
-static_assert(MAX_STACKS_PER_CHANNEL > 0, "At least 1 stack is required per router");
 
 // The number of bytes needed to hold an APDU fully encapsulated with TPDU/LPDU
 static const uint32_t APDU_LPDU_BUFFER_SIZE = (((MAX_TX_APDU_SIZE / 249) + 1) * 292);
 
 // default sizing is big enough to receive a full APDU with full LPDU's
 static const uint32_t LINK_RECEIVER_BUFFER_SIZE = APDU_LPDU_BUFFER_SIZE;
+
+static_assert(MAX_TX_APDU_SIZE >= 100, "APDU tx buffer size must be at least 100");
+static_assert(MAX_RX_APDU_SIZE >= 249, "APDU rx buffer size must be at least 249 bytes");
+static_assert(DEFAULT_APDU_BUFFER_SIZE <= MAX_TX_APDU_SIZE, "default buffer size must be less than the maximum");
+static_assert(MAX_STACKS_PER_CHANNEL > 0, "At least 1 stack is required per router");
 static_assert(LINK_RECEIVER_BUFFER_SIZE >= 292, "Receiver must buffer at least 292 bytes");
+
+
 }
 }
 
