7463fd101398fdda49d724d76c7a819373bfaaef
Adam Crain <jadamcrain@gmail.com>
2014-07-07 20:14:43 -0400

    analog output status conversions now use overrange flag automatically

--- cpp/libs/opendnp3/src/opendnp3/app/WriteConversions.h
+++ cpp/libs/opendnp3/src/opendnp3/app/WriteConversions.h
@@ -130,10 +130,9 @@ typedef ConvertQV<Group32Var6, Analog> ConvertGroup32Var6;
 typedef ConvertQVTRangeCheck<Group32Var7, Analog, 0x20> ConvertGroup32Var7;
 typedef ConvertQVT<Group32Var8, Analog> ConvertGroup32Var8;
 
-// Group 40 - TODO - strange to have no flag
-typedef ConvertQVRangeCheck<Group40Var1, AnalogOutputStatus, 0> ConvertGroup40Var1;
-typedef ConvertQVRangeCheck<Group40Var2, AnalogOutputStatus, 0> ConvertGroup40Var2;
-typedef ConvertQVRangeCheck<Group40Var3, AnalogOutputStatus, 0> ConvertGroup40Var3;
+typedef ConvertQVRangeCheck<Group40Var1, AnalogOutputStatus, 0x20> ConvertGroup40Var1;
+typedef ConvertQVRangeCheck<Group40Var2, AnalogOutputStatus, 0x20> ConvertGroup40Var2;
+typedef ConvertQVRangeCheck<Group40Var3, AnalogOutputStatus, 0x20> ConvertGroup40Var3;
 typedef ConvertQV<Group40Var4, AnalogOutputStatus> ConvertGroup40Var4;
 
 // Group 41
