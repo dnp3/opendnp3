d212aab3d723a95703165ed99aba61debb29eed2
Darren Wiley <coder007@hotmail.co.uk>
2014-08-11 16:57:48 +0100

    Fixed Analog time in ctor.

--- dotnet/bindings/CLRInterface/MeasurementTypes.cs
+++ dotnet/bindings/CLRInterface/MeasurementTypes.cs
@@ -144,7 +144,8 @@ namespace Automatak.DNP3.Interface
         /// <param name="value">value of the measurement</param>
         /// <param name="quality">quality enumeration as a bitfield</param>
         /// <param name="time">timestamp</param>
-        public Analog(double value, byte quality, DateTime time) : base(value, quality, DateTime.MinValue)
+        public Analog(double value, byte quality, DateTime time)
+            : base(value, quality, time)
 	    {}
 
         /// <summary>
