2227d168eeb51cc8eb52989e7dd85e04168502c4
Adam Crain <jadamcrain@gmail.com>
2014-08-27 07:56:02 -0400

    added an executable project for the outstation

---
+++ embedded/atmeldue/demooutstation/demooutstation.cpp
@@ -0,0 +1,24 @@
+/*
+ * demooutstation.cpp
+ *
+ * Created: 8/27/2014 7:55:11 AM
+ *  Author: jadamcrain
+ */ 
+
+#include "sam.h"
+
+/**
+ * \brief Application entry point.
+ *
+ * \return Unused (ANSI-C compatibility).
+ */
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    while (1) 
+    {
+        //TODO:: Please write your application code 
+    }
+}
