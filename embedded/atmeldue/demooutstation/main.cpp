df51ee0c50b3abd89f2fa37c02afcfff4c93cd87
Adam Crain <jadamcrain@gmail.com>
2014-09-18 04:48:17 -0400

    working serial echo via interrupt

--- embedded/atmeldue/demooutstation/main.cpp
+++ embedded/atmeldue/demooutstation/main.cpp
@@ -25,9 +25,9 @@
 using namespace opendnp3;
 using namespace openpal;
 
-void ToggleLEDEvery3Seconds(IExecutor* pExecutor);
+void ToggleLED(IExecutor* pExecutor);
 
-uint32_t led = (1u << 27); 
+const uint32_t LED_MASK = (1u << 27); 
 
 const uint32_t MAX_FRAG_SIZE = 2048;
 
@@ -39,18 +39,92 @@ void SysTick_Handler(void)
 	gpExecutor->Tick();		
 }
 
-/**
- * \brief Application entry point.
- *
- * \return Unused (ANSI-C compatibility).
- */
+void configure_uart(void)
+{
+	uint32_t ul_sr;
+	
+	// ==> Pin configuration
+	// Disable interrupts on Rx and Tx
+	PIOA->PIO_IDR = PIO_PA8A_URXD | PIO_PA9A_UTXD;
+	
+	// Disable the PIO of the Rx and Tx pins so that the peripheral controller can use them
+	PIOA->PIO_PDR = PIO_PA8A_URXD | PIO_PA9A_UTXD;
+	
+	// Read current peripheral AB select register and set the Rx and Tx pins to 0 (Peripheral A function)
+	ul_sr = PIOA->PIO_ABSR;
+	PIOA->PIO_ABSR &= ~(PIO_PA8A_URXD | PIO_PA9A_UTXD) & ul_sr;
+	
+	// Enable the pull up on the Rx and Tx pin
+	PIOA->PIO_PUER = PIO_PA8A_URXD | PIO_PA9A_UTXD;
+	
+	// ==> Actual uart configuration
+	// Enable the peripheral uart controller
+	PMC->PMC_PCER0 = 1 << ID_UART;
+	
+	// Reset and disable receiver & transmitter
+	UART->UART_CR = UART_CR_RSTRX | UART_CR_RSTTX | UART_CR_RXDIS | UART_CR_TXDIS;
+	
+	// Set the baudrate to 115200
+	UART->UART_BRGR = 45; // 84000000 / 16 * x = BaudRate (write x into UART_BRGR)
+	
+	// No Parity
+	UART->UART_MR = UART_MR_PAR_NO;
+	
+	// Disable PDC channel requests
+	UART->UART_PTCR = UART_PTCR_RXTDIS | UART_PTCR_TXTDIS;
+	
+	// Disable / Enable interrupts on end of receive
+	UART->UART_IDR = 0xFFFFFFFF;
+	NVIC_EnableIRQ((IRQn_Type) ID_UART);
+	UART->UART_IER = UART_IER_RXRDY;
+	
+	// Enable receiver and trasmitter
+	UART->UART_CR = UART_CR_RXEN | UART_CR_TXEN;	
+}
+
+int uart_getchar(uint8_t *c)
+{
+    // Check if the receiver is ready
+    if((UART->UART_SR & UART_SR_RXRDY) == 0)
+    return 1;
+ 
+    // Read the character
+    *c = (uint8_t) UART->UART_RHR;
+    return 0;
+}
+ 
+int uart_putchar(const uint8_t c)
+{
+    // Check if the transmitter is ready
+    if(!(UART->UART_SR & UART_SR_TXRDY))
+    return 1;
+ 
+    // Send the character
+    UART->UART_THR = c;
+    return 0;
+}
+
+void UART_Handler(void)
+{
+	uint8_t c;
+	
+	// Check if the interrupt source is receive ready
+	if(UART->UART_IMR & UART_IMR_RXRDY)
+	{
+		if(uart_getchar(&c) == 0)
+		{
+			uart_putchar(c);
+		}
+	}
+}
+
 int main(void)
 {
     // Initialize the SAM system
     SystemInit();
 		
 	// Output Enable Register
-	REG_PIOB_OER = led; 	
+	REG_PIOB_OER = LED_MASK; 	
 		
 	ExecutorImpl exe(5,5);
 	gpExecutor = &exe;
@@ -89,18 +163,22 @@ int main(void)
 		
 	// Setup SysTick Timer for 1 msec interrupts
 	if (SysTick_Config(84000000 / 1000)) {
-		while (1) {  //
+		while (1) {  // trap error
 		}
 	}
+	
+	configure_uart();
 	
 	// enable USART RX/TX interrupts
 	parser.Init();	
 	
-	ToggleLEDEvery3Seconds(&exe);				
+	ToggleLED(&exe);				
 	
 	for (;;)
 	{		
 		exe.Run();
+		
+		parser.ProcessRx();
 		
 		// TODO - put the device into a sleep mode that the system timer or usart can wake it from
 	}
@@ -108,21 +186,21 @@ int main(void)
 	return 0;
 }
 
-void ToggleLEDEvery3Seconds(IExecutor* pExecutor)
+void ToggleLED(IExecutor* pExecutor)
 {	
 	// Toggle the LED
 	if(gValue)
 	{
-		REG_PIOB_SODR = led; // Set Output Data Register, turns LED on
+		REG_PIOB_SODR = LED_MASK; // Set Output Data Register, turns LED on
 	}
 	else
 	{
-		REG_PIOB_CODR = led; // Clear Output Data Register, turns LED off
+		REG_PIOB_CODR = LED_MASK; // Clear Output Data Register, turns LED off
 	}
 	gValue = !gValue;
 		
 	// schedule the next toggle
-	auto lambda = [pExecutor]() { ToggleLEDEvery3Seconds(pExecutor); };
-	pExecutor->Start(TimeDuration::Seconds(3), Action0::Bind(lambda));
+	auto lambda = [pExecutor]() { ToggleLED(pExecutor); };
+	pExecutor->Start(TimeDuration::Milliseconds(200), Action0::Bind(lambda));
 }
 
