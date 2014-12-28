b41f24a0560b3d0d6b8017df78517df1c17cfe1a
Adam Crain <jadamcrain@gmail.com>
2014-12-28 16:50:10 -0500

    timestamp mode is now byte type

--- generation/dnp3/src/main/scala/com/automatak/render/dnp3/enums/TimestampMode.scala
+++ generation/dnp3/src/main/scala/com/automatak/render/dnp3/enums/TimestampMode.scala
@@ -6,12 +6,12 @@ object TimestampMode {
 
   private val comments = List("Indicates the validity of a timestamp values for an entire object header")
 
-  def apply(): EnumModel = EnumModel("TimestampMode", comments, EnumModel.Integer, codes, Base10)
+  def apply(): EnumModel = EnumModel("TimestampMode", comments, EnumModel.UInt8, codes, Base10)
 
   private val codes = List(
     EnumValue("INVALID", 0, "Timestamp is not valid, ignore the value and use a local timestamp"),
-    EnumValue("SYNCHRONIZED", 1, "The timestamp is UTC synchronzied at the remote device"),
-    EnumValue("UNSYNCHRONIZED", 2, "The device indicate the timstamp may be unsynchronized")
+    EnumValue("SYNCHRONIZED", 1, "The timestamp is UTC synchronized at the remote device"),
+    EnumValue("UNSYNCHRONIZED", 2, "The device indicate the timestamp may be unsynchronized")
   )
 
 }
