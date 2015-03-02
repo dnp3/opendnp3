22cbf843497723322a9605310aa702daa21f879d
Adam Crain <jadamcrain@gmail.com>
2015-03-02 09:57:25 -0500

    comment for API change

--- cpp/libs/asiodnp3/src/asiodnp3/DNP3Manager.h
+++ cpp/libs/asiodnp3/src/asiodnp3/DNP3Manager.h
@@ -82,6 +82,7 @@ public:
 	* @param minOpenRetry minimum connection retry interval on failure in milliseconds
 	* @param maxOpenRetry minimum connection retry interval on failure in milliseconds
 	* @param host IP address of remote outstation (i.e. 127.0.0.1 or www.google.com)
+	* @param local adapter address on which to attempt the connection (use 0.0.0.0 for all adapters)
 	* @param port Port of remote outstation is listening on
 	* @param strategy Reconnection delay strategy, default to exponential
 	*/
