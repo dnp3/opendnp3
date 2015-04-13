f613a72ce2c0b2e3a1e44b0e2c0871ace7efd660
jadamcrain <jadamcrain@gmail.com>
2015-04-13 10:48:28 -0700

    asdu fuzzer is running

--- cpp/tests/fuzzer/Bindings.cpp
+++ cpp/tests/fuzzer/Bindings.cpp
@@ -1,6 +1,32 @@
 
 #include <cstddef>
+#include <iostream>
 
-extern "C" void TestOneInput(const unsigned char *data, size_t size) {
-  
+#include <opendnp3/app/parsing/APDUParser.h>
+#include <openpal/logging/LogRoot.h>
+
+using namespace openpal;
+using namespace opendnp3;
+
+class ConsoleLogger : public openpal::ILogHandler
+{
+
+public:	
+	virtual void Log(const openpal::LogEntry& entry) override final
+	{
+		std::cout << entry.GetMessage() << std::endl;
+	}
+};
+
+static ConsoleLogger handler;
+
+static LogRoot root(&handler, "logger", LogFilters(~0));
+
+extern "C" void TestOneInput(const unsigned char *data, size_t size) 
+{
+
+  ReadBufferView input(data, size);
+  auto logger = root.GetLogger();
+  //APDUParser::ParseAndLogAll(input, &logger);
+  APDUParser::ParseAndLogAll(input, nullptr);
 }
