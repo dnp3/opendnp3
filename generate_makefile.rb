9468b13a9a028805c6fa88e8cdb6aee62dd79047
jadamcrain <jadamcrain@gmail.com>
2015-07-06 08:32:38 -0700

    Added erb-based system for generating Makefile.am

---
+++ generate_makefile.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+
+def get_files(pattern)  
+  `ls #{pattern} -1`.split("\n").join(" \\\n")  
+end
+
+# library cpp
+openpal_sources = get_files('cpp/libs/openpal/src/openpal/**/*.cpp')
+asiopal_sources = get_files('cpp/libs/asiopal/src/asiopal/*.cpp')
+opendnp3_sources = get_files('cpp/libs/opendnp3/src/opendnp3/**/*.cpp')
+asiodnp3_sources = get_files('cpp/libs/asiodnp3/src/asiodnp3/*.cpp')
+
+# tests
+openpal_test_sources = get_files('cpp/tests/openpaltests/src/*.cpp')
+opendnp3_test_sources = get_files('cpp/tests/opendnp3tests/src/*.cpp')
+
+template_string = File.read('Makefile.am.erb')
+template  = ERB.new template_string
+File.write('Makefile.am', template.result)
