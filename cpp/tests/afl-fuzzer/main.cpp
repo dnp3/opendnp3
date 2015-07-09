69d9d176821dd6d9cb4cc886f6f479b363f518aa
jadamcrain <jadamcrain@gmail.com>
2015-07-09 13:40:59 -0700

    afl binding now working

--- cpp/tests/afl-fuzzer/main.cpp
+++ cpp/tests/afl-fuzzer/main.cpp
@@ -1,5 +1,6 @@
 
 #include <cstddef>
+#include <cstdio>
 #include <iostream>
 
 #include <opendnp3/app/parsing/APDUParser.h>
@@ -20,11 +21,16 @@ public:
 
 int main(int argc, char* argv[])
 {
-	std::cout << "argc: " << argc << std::endl;
-  
+	std::cout << "argc: " << argc << std::endl;	
   
 	uint8_t buffer[4096];
-	ReadBufferView view(buffer, 4096);
+		
+	//try to read from stdin
+	size_t num = fread(buffer, 1, 4096, stdin);
+	
+	std::cout << "read: " << num << std::endl;
+	
+	ReadBufferView view(buffer, static_cast<uint32_t>(num));
 	
 	ConsoleLogger handler;
 	LogRoot root(&handler, "log", ~0);
