94f0a147554550e4e47bef0313cf2d9169824ded
Adam Crain <jadamcrain@gmail.com>
2015-09-03 13:23:41 -0400

    Added ConsoleLogger to public includes

--- cpp/libs/src/asiodnp3/ConsoleLogger.cpp
+++ cpp/libs/src/asiodnp3/ConsoleLogger.cpp
@@ -18,7 +18,7 @@
  * may have been made to this file. Automatak, LLC licenses these modifications
  * to you under the terms of the License.
  */
-#include "ConsoleLogger.h"
+#include "asiodnp3/ConsoleLogger.h"
 
 #include <chrono>
 #include <sstream>
