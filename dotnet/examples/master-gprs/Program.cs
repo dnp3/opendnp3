17306e337ed793ff09e32a2400b3d78129fd40e2
Adam Crain <jadamcrain@gmail.com>
2016-02-09 14:55:20 -0500

    Added a demo program for the C# GPRS master

---
+++ dotnet/examples/master-gprs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Automatak.DNP3.Interface;
+using Automatak.DNP3.Adapter;
+
+namespace DotNetMasterGPRSDemo
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var manager = DNP3SessionManagerFactory.CreateManager(4, PrintingLogAdapter.Instance);
+
+            Console.WriteLine("Enter a command");
+
+            while (true)
+            {
+                switch (Console.ReadLine())
+                {                    
+                    case "x":
+                        return 0;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
