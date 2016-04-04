432cbe5b680f86c66fb3fe66f29f7279f45d3ce8
Adam Crain <jadamcrain@gmail.com>
2016-04-04 10:54:59 -0400

    Added an integration test that tests the correctness of a stream of analog event values

--- cpp/tests/asiodnp3tests/src/TestEventIntegration.cpp
+++ cpp/tests/asiodnp3tests/src/TestEventIntegration.cpp
@@ -43,18 +43,32 @@ class EventReceiver final : public ISOEHandler {
 
 public:
 
-	EventReceiver() : m_sequence(0)
+	EventReceiver(uint32_t numToSend, uint32_t maxOutstanding, std::uint16_t numValues) :
+		m_rx_sequence(0),
+		m_tx_sequence(0),
+		m_num_remaining(numToSend),
+		m_num_outstanding(0),
+		MAX_OUTSTANDING(maxOutstanding),
+		NUM_VALUES(numValues)
 	{}
 
 	virtual void Start() override { m_mutex.lock(); }
-	virtual void End()  override { 
-		m_mutex.unlock();
+	virtual void End()  override { 		
+		m_mutex.unlock();		
 		m_condition.notify_one();
 	}
 	
 	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<Binary>>& values) override {}
 	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<DoubleBitBinary>>& values) override {}
-	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<Analog>>& values) override {}
+	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<Analog>>& values) override 
+	{		
+		auto record = [this](const Indexed<Analog>& item) {
+			m_rx_values.push_back(item);			
+		};
+
+		values.ForeachItem(record);		
+	}
+	
 	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<Counter>>& values) override {}
 	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<FrozenCounter>>& values) override {}
 	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<BinaryOutputStatus>>& values) override {}
@@ -65,30 +79,82 @@ public:
 	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<AnalogCommandEvent>>& values) override  {}
 	virtual void Process(const HeaderInfo& info, const ICollection<Indexed<SecurityStat>>& values) override {}
 
-	bool LoadAndWait(std::chrono::monotonic_clock::duration timeout) {
-		std::unique_lock<std::mutex> lock(m_mutex);
+	bool LoadAndWait(IOutstation* outstation, std::chrono::monotonic_clock::duration timeout) {
+		
+		std::unique_lock<std::mutex> lock(m_mutex);		
+		
+		const auto RX_NUM = this->ProcessRxValues();
+		
+		const auto TX_NUM = MAX_OUTSTANDING - m_num_outstanding;
+		this->LoadNewValues(outstation, TX_NUM);		
+			
+		if (m_condition.wait_for(lock, timeout) == cv_status::timeout) {
+			throw std::exception("timed out waiting for update");
+		}
 
-
-
-		return m_condition.wait_for(lock, timeout) != cv_status::timeout;
+		return m_num_remaining == 0;
 	}
 
 private:
 
-	std::vector<Indexed<Analog>> m_values;
+	uint32_t ProcessRxValues()
+	{		
+		for (auto& value : m_rx_values) {
+			
+			if (value.value.value != m_rx_sequence) {
+				throw std::exception("Unexpected rx value");
+			}
 
-	std::uint32_t m_sequence;
+			if (value.index != (m_rx_sequence%100)) {
+				throw std::exception("Unexpected rx value");
+			}
+
+			++m_rx_sequence;
+		}
+
+		const auto NUM = m_rx_values.size();
+		m_num_remaining -= NUM;
+		m_num_outstanding -= NUM;
+		m_rx_values.clear();
+		return NUM;
+	}
+
+	void LoadNewValues(IOutstation* outstation, uint32_t num)
+	{
+		MeasUpdate tx(outstation);
+
+		for (uint32_t i = 0; i < num; ++i) {
+			Analog a(m_tx_sequence);
+			tx.Update(a, m_tx_sequence % 100);
+			++m_tx_sequence;			
+		}		
+
+		m_num_outstanding += num;
+	}
+
+	std::vector<Indexed<Analog>> m_rx_values;
+
+	std::uint32_t m_tx_sequence;
+	std::uint32_t m_rx_sequence;
+	
+
+	std::uint32_t m_num_remaining;
+	uint32_t m_num_outstanding;
+	
+	const uint32_t MAX_OUTSTANDING;	
+	const std::uint16_t NUM_VALUES;
+	
 	std::mutex m_mutex;
 	std::condition_variable m_condition;
 };
 
-IOutstation* ConfigureOutstation(DNP3Manager& manager, int levels)
+IOutstation* ConfigureOutstation(DNP3Manager& manager, int levels, uint16_t numValues, uint16_t eventBufferSize)
 {
 	auto server = manager.AddTCPServer("server", levels, ChannelRetry::Default(), "127.0.0.1", 20000);
 
 	OutstationStackConfig stackConfig;	
-	stackConfig.dbTemplate = DatabaseTemplate::AllTypes(100);
-	stackConfig.outstation.eventBufferConfig = EventBufferConfig::AllTypes(100);
+	stackConfig.dbTemplate = DatabaseTemplate::AllTypes(numValues);
+	stackConfig.outstation.eventBufferConfig = EventBufferConfig::AllTypes(eventBufferSize);
 	stackConfig.outstation.params.allowUnsolicited = true;
 
 	auto outstation = server->AddOutstation("outstation", SuccessCommandHandler::Instance(), DefaultOutstationApplication::Instance(), stackConfig);
@@ -113,18 +179,19 @@ TEST_CASE(SUITE("TestEventIntegration"))
 {
 	const auto LEVELS = levels::NORMAL | flags::APP_HEADER_RX | flags::APP_HEADER_TX | flags::APP_OBJECT_RX | flags::APP_OBJECT_TX;
 
-	EventReceiver eventrx;
+	const uint32_t NUM_TO_SEND = 100000;		// send 100,000 values
+	const uint32_t MAX_OUTSTANDING = 40;		// important to keep this below the event buffer size, otherwise we can overflow it and lose events
+	const uint16_t NUM_VALUES = 100;			// size of the static database. we'll rotate through indices
+	const uint16_t EVENT_BUFFER_SIZE = 100;		// size of the events buffers for all types
+
+	EventReceiver eventrx(NUM_TO_SEND, MAX_OUTSTANDING, NUM_VALUES);
 
 	DNP3Manager manager(2);
-	manager.AddLogSubscriber(ConsoleLogger::Instance());		
+	//manager.AddLogSubscriber(ConsoleLogger::Instance());		
 
-	auto outstation = ConfigureOutstation(manager, LEVELS);
+	auto outstation = ConfigureOutstation(manager, LEVELS, NUM_VALUES, EVENT_BUFFER_SIZE);
 	auto master = ConfigureMaster(manager, eventrx, LEVELS);
 
-	for(int i=0; i<100; ++i) {
-		std::this_thread::sleep_for(std::chrono::seconds(1));
-		MeasUpdate tx(outstation);
-		tx.Update(Analog(i), 0);
-	}
+	while (!eventrx.LoadAndWait(outstation, std::chrono::seconds(60)));
 }
 
