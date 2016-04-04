93dd60bea35402624ab3ff033aa642a45d4db673
Adam Crain <jadamcrain@gmail.com>
2016-04-04 12:08:09 -0400

    Remove optional ASIO configuration for msvc

--- cmake/inc/msvc.cmake
+++ cmake/inc/msvc.cmake
@@ -1,12 +1,8 @@
 if (WIN32)
+		
+	add_definitions(-D_WIN32_WINNT=0x0501)
+	add_definitions(-DASIO_HAS_STD_SYSTEM_ERROR)  
 	
-	if (USING_ASIO)
-		add_definitions(-D_WIN32_WINNT=0x0501)
-		add_definitions(-DASIO_HAS_STD_SYSTEM_ERROR)  
-	else ()
-		set(WINSOCKAPI "-D_WINSOCKAPI_")
-		set(PLATFORM_SOCKET_LIBRARIES ws2_32)
-	endif()
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /MP")
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_SCL_SECURE_NO_WARNINGS -D_CRT_SECURE_NO_WARNINGS ${WINSOCKAPI} /wd4251 /wd4996 /wd4273 /wd4275 /wd4018 /wd4146 /EHsc")
 
