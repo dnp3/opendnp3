75aef51e8388a0ef2e46839e78007067ba36996c
Adam Crain <jadamcrain@gmail.com>
2016-07-25 15:53:54 -0400

    Modify CMake build to first look for ASIO as a submodule

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -2,6 +2,5 @@ get_filename_component(settings_cmake_dir "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 include("${settings_cmake_dir}/inc/msvc.cmake")
 include("${settings_cmake_dir}/inc/posix.cmake")
-include("${settings_cmake_dir}/inc/asio.cmake")
 
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
