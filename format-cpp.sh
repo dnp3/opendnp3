28c026551a7726649a397078e486a78050a8bec7
Adam Crain <jadamcrain@gmail.com>
2016-09-20 10:31:15 -0400

    remove shell script for formatting as this is now a target

--- format-cpp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-astyle -R ./cpp/*.h ./cpp/*.cpp --options=./config/astyle.cfg --exclude=./cpp/libs/include/opendnp3/gen --exclude=./cpp/libs/src/opendnp3/gen --exclude=./cpp/libs/src/opendnp3/objects
-
