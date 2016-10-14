06a055c76a2b633bdd3ebc94ec1593ff08854d4e
jadamcrain <jadamcrain@gmail.com>
2016-10-14 07:56:20 -0700

    don't add extra debug flags for coverae. require users to also set debug build

--- cmake/inc/posix.cmake
+++ cmake/inc/posix.cmake
@@ -38,7 +38,7 @@ if (UNIX)
 	endif ()
 
 	if(COVERAGE)
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage -g -O0")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --coverage")
 	endif()
 
 	if (WERROR)
