e3839ad618c9e77a8ba20264527346bf9e4ba4b0
Philippe Antoine <contact@catenacyber.fr>
2018-05-21 16:12:57 +0200

    Fuzz target for dnp3 decoder

---
+++ cpp/tests/fuzz/fuzzdnp3.cpp
@@ -0,0 +1,22 @@
+#include <openpal/logging/Logger.h>
+#include <openpal/container/Buffer.h>
+
+#include <dnp3decode/Decoder.h>
+#include <opendnp3/LogLevels.h>
+#include <asiodnp3/ConsoleLogger.h>
+
+using namespace std;
+using namespace openpal;
+using namespace opendnp3;
+using namespace asiodnp3;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    RSlice buffer(Data, Size);
+    openpal::Logger logger(ConsoleLogger::Create(), "decoder", LogFilters(0));
+    IDecoderCallbacks callback;
+    Decoder decoder(callback, logger);
+
+    decoder.DecodeLPDU(buffer);
+
+    return 0;
+}
