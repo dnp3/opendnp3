acfbfbcdab1b7932e72251221a96c9b65df99134
jadamcrain <jadamcrain@gmail.com>
2019-06-07 14:08:02 -0700

    adjust public Java API to match 3.0.0 changes

--- java/bindings/src/main/java/com/automatak/dnp3/mock/PrintingSOEHandler.java
+++ java/bindings/src/main/java/com/automatak/dnp3/mock/PrintingSOEHandler.java
@@ -32,15 +32,13 @@ public class PrintingSOEHandler implements SOEHandler {
     private PrintingSOEHandler(){}
 
     @Override
-    public void start()
-    {
-        System.out.println("start asdu");
+    public void beginFragment(ResponseInfo info) {
+        System.out.println("begin response: " + info.toString());
     }
 
     @Override
-    public void end()
-    {
-        System.out.println("end asdu");
+    public void endFragment(ResponseInfo info) {
+        System.out.println("end response: " + info.toString());
     }
 
     @Override
